610048af21185a2d1131b198e9d735ffe2cbdbf1
kimhyonseong <corea6524@gmail.com>
2022-07-23 23:03:27 +0900

    예외처리 실제 사례

--- exception/src/main/java/com/example/exception/controller/RestApiController.java
+++ exception/src/main/java/com/example/exception/controller/RestApiController.java
@@ -3,16 +3,26 @@ package com.example.exception.controller;
 import com.example.exception.dto.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/user")
+@Validated
 public class RestApiController {
     @GetMapping("")
-    public User get(@RequestParam(required = false) String name,@RequestParam(required = false) Integer age) {
+    public User get(
+            @Size(min = 2)
+            @RequestParam String name,
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age) {
         User user = new User();
         user.setName(name);
         user.setAge(age);
@@ -27,9 +37,9 @@ public class RestApiController {
         return user;
     }
 
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity MethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        System.out.println("api controller");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
+//    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+//    public ResponseEntity MethodArgumentNotValidException(MethodArgumentNotValidException e) {
+//        System.out.println("api controller");
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+//    }
 }

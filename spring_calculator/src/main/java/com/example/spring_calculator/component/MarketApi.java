ad5c6fb7457793e23c5c603f672de3e2cdd78f36
kimhyonseong <corea6524@gmail.com>
2022-10-13 16:38:30 +0900

    rest api test with mock

---
+++ spring_calculator/src/main/java/com/example/spring_calculator/component/MarketApi.java
@@ -0,0 +1,10 @@
+package com.example.spring_calculator.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MarketApi {
+    public int connect() {
+        return 1100;
+    }
+}
---
+++ spring_calculator/src/test/java/com/example/spring_calculator/controller/CalculatorApiControllerTest.java
@@ -0,0 +1,78 @@
+package com.example.spring_calculator.controller;
+
+import com.example.spring_calculator.component.Calculator;
+import com.example.spring_calculator.component.DollarCalculator;
+import com.example.spring_calculator.component.MarketApi;
+import com.example.spring_calculator.dto.Req;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+// 필요한 것만 로딩 -> 자원 줄임
+@WebMvcTest(CalculatorApiController.class)
+@AutoConfigureWebMvc
+@Import({DollarCalculator.class, Calculator.class})
+public class CalculatorApiControllerTest {
+    @MockBean
+    private MarketApi marketApi;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    // 테스트할때마다 초기화
+    @BeforeEach
+    public void init() {
+        Mockito.when(marketApi.connect()).thenReturn(3000);
+    }
+
+
+    //get test
+    @Test
+    public void sumTest() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("http://localhost:8080/api/sum")
+                        .queryParam("x","10")
+                        .queryParam("y","10")
+        ).andExpect(
+                // 200인지 확인
+                MockMvcResultMatchers.status().isOk()
+        ).andExpect(
+                MockMvcResultMatchers.content().string("60000")
+        ).andDo(MockMvcResultHandlers.print());
+    }
+
+    //post test
+    @Test
+    public void minusTest() throws Exception {
+        Req req = new Req();
+        req.setX(10);
+        req.setY(10);
+
+        String json = new ObjectMapper().writeValueAsString(req);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("http://localhost:8080/api/minus")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.result").value("0")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.response.resultCode").value("OK")
+                )
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
